7a21a125a6f09d4a0b1c0b6cd5db8c5d671c7a7d
nicolaslabbe <nicolas.labbe@adfab.fr>
2017-11-10 08:19:12 +0100

    refactor everything into one route

--- crontasks/disruptions.js
+++ crontasks/disruptions.js
@@ -4,44 +4,44 @@ const Utils = require("../Utils");
 const Libs = require("../Libs");
 
 const sendPushString = () => {
-  var promises = [];
-  var results = [];
-  var errors = [];
-  Array.prototype.forEach.call(JSON.parse(process.env.TRAIN_STOPS), function(
-    item
-  ) {
-    promises.push(
-      Libs.sncf.find(item.id, item.direction, process.env.TRAIN_BEARER).then(
-        function(res) {
-          results.push(res);
-        },
-        function(error) {
-          errors.push(error);
-        }
-      )
-    );
-  });
+  // var promises = [];
+  // var results = [];
+  // var errors = [];
+  // Array.prototype.forEach.call(JSON.parse(process.env.TRAIN_STOPS), function(
+  //   item
+  // ) {
+  //   promises.push(
+  //     Libs.sncf.find(item.id, item.direction, process.env.TRAIN_BEARER).then(
+  //       function(res) {
+  //         results.push(res);
+  //       },
+  //       function(error) {
+  //         errors.push(error);
+  //       }
+  //     )
+  //   );
+  // });
 
-  Promise.all(promises)
-    .then(function() {
-      var pushString = "";
-      Array.prototype.forEach.call(results, function(items) {
-        Array.prototype.forEach.call(items, function(item) {
-          if (item && item.status) {
-            pushString += item.status;
-          }
-        });
-      });
-      if (pushString !== "") {
-        Libs.push.send(pushString);
-      } else {
-        Libs.push.send("traffic ok");
-      }
-      // Libs.status.success(res, results);
-    })
-    .catch(function(e) {
-      console.log("e", e);
-    });
+  // Promise.all(promises)
+  //   .then(function() {
+  //     var pushString = "";
+  //     Array.prototype.forEach.call(results, function(items) {
+  //       Array.prototype.forEach.call(items, function(item) {
+  //         if (item && item.status) {
+  //           pushString += item.status;
+  //         }
+  //       });
+  //     });
+  //     if (pushString !== "") {
+  //       Libs.push.send(pushString);
+  //     } else {
+  //       Libs.push.send("traffic ok");
+  //     }
+  //     // Libs.status.success(res, results);
+  //   })
+  //   .catch(function(e) {
+  //     console.log("e", e);
+  //   });
 };
 
 const schedule = () => {
